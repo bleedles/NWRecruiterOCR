f132806e1af2e227ea6e6e22c839eb957fc2c723
bleedles <blake.needleman@gmail.com>
2017-07-21 06:07:29 -0400

    UI

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -3,9 +3,40 @@
 
   angular
     .module('core')
-    .controller('HomeController', HomeController);
+    .controller('HomeController', ['$scope', 'boxDataService', function($scope, boxDataService)
+    {
 
-  function HomeController() {
-    var vm = this;
-  }
+      $scope.applicants = [
+        {name: "Sophia Knowles", skillset:["AngularJS", "SQL"]}, 
+        {name: "Jordan Hanson", skillset:[".NET", "MongoDB"]},
+        {name: "Blake Needleman", skillset:["Javascript", ".NET", "BOX"]},
+        {name: "Josh Martin", skillset:["Negotiation", "HTML"]},
+        {name: "Torrelle Howard", skillset:["Javascript", ".NET", "MongoDB"]}
+      ];
+
+
+      $scope.qualifiedApplicants = [];
+
+      $scope.skills = ["Java", "Javascript", ".NET", "MongoDB"];
+      $scope.searchSkillset = function()
+      {
+        boxDataService.postKeywords([$scope.searchFilter]).then(function(response)
+        {
+          $scope.files = response.data.files;
+        },
+        function (response)
+        {
+          // fail  
+        });
+        $scope.qualifiedApplicants = [];
+        angular.forEach($scope.applicants, function(value, index)
+        {
+          if (value.skillset == $scope.chosenSkillSet)
+          {
+            $scope.qualifiedApplicants.push(value);
+            //console.log(value.name);
+          }
+        })
+      }
+    }]);
 }());
--- modules/core/client/services/box.client.service.js
+++ modules/core/client/services/box.client.service.js
@@ -1,82 +1,24 @@
 (function () {
   'use strict';
 
-  angular
-    .module('core')
-    .factory('BoxService', BoxService);
+ angular
+     .module('core')
+    .factory('boxDataService', boxDataService);
 
-  BoxService.$inject = ['$resource', '$log', '$stateParams'];
+    boxDataService.$inject = ['$http'];
+    function boxDataService($http) {
+    
+        var boxFactoryData = {};
 
-  function BoxService($resource, $log, $stateParams) {
-    var TeamRequests = $resource('/api/teamRequests/:teamRequestId', {
-      teamRequestId: '@_id'
-    }, {
-      update: {
-        method: 'PUT'
-      },
-      accept: {
-        url: '/api/teamRequests/accept/:teamRequestToken',
-        method: 'GET',
-        params: {
-          teamRequestToken: function() {
-            return $stateParams.teamRequestToken;
-          } 
-        },
-        hasBody: true
-      },
-      reject: {
-        url: '/api/teamRequests/reject/:teamRequestToken',
-        method: 'GET',
-        params: {
-          teamRequestToken: function() {
-            return $stateParams.teamRequestToken;
-          } 
-        },
-        hasBody: true
+        boxFactoryData.postKeywords = function(keywords)
+        {
+          return  $http({
+                method: 'POST',
+                url: 'https://nwrecruiter.azurewebsites.net/api/files',
+                data: {'keywords': keywords}
+            });
+        };
+
+        return boxFactoryData;
       }
-    });
-
-    angular.extend(TeamRequests.prototype, {
-      createOrUpdate: function () {
-        var teamRequest = this;
-        return createOrUpdate(teamRequest);
-      }
-    });
-
-    angular.extend(TeamRequests, {
-      acceptRequest: function() {
-        return this.accept().$promise;
-      },
-      rejectRequest: function() {
-        return this.reject().$promise;
-      }
-    });
-
-    return TeamRequests;
-
-    function createOrUpdate(teamRequest) {
-      if (teamRequest._id) {
-        return teamRequest.$update(onSuccess, onError);
-      } else {
-        return teamRequest.$save(onSuccess, onError);
-      }
-
-      // Handle successful response
-      function onSuccess(teamRequest) {
-        // Any required internal processing from inside the service, goes here.
-      }
-
-      // Handle error response
-      function onError(errorResponse) {
-        var error = errorResponse.data;
-        // Handle error internally
-        handleError(error);
-      }
-    }
-
-    function handleError(error) {
-      // Log error
-      $log.error(error);
-    }
-  }
 }());
